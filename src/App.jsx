867f651748fb06991f8a0c2e254c103f922da51f
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-16 10:48:56 +0500

    update app.js , store.js, counterSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
+  const count = useSelector((state) => state.count)
   return (
-    <div>App</div>
+    <div>
+      <h2>Counter App - Redux Toolkit</h2>
+      <h4>Count: {count}</h4>
+    </div>
   )
 }
 
--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -6,11 +6,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = { count: 0 };
 
 const counterSlice = createSlice({
-    name: counter,
+    name: 'counter',
     initialState,
     reducers: {
-        increment: () => { },
-        decrement: () => { },
-        reset: () => { },
+        increment: (state) => { state.count += 1 },
+        decrement: (state) => { state.count -= 1 },
+        reset: (state) => { state.count = 0 },
     }
-})
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+//this method is called object destructuring, 
